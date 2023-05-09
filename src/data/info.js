91db990011a5d2f8713de726721d00ffeb6b02ce
Reander Agulto <reanderagulto29@gmail.com>
2023-05-09 21:58:43 +0800

    Added info.js for placeholder data

---
+++ src/data/info.js
@@ -0,0 +1,132 @@
+export const pageInfo = {
+    banner: {
+        title: 'Jeni and Reander', 
+        subtitle: 'are getting married', 
+        date: 'September 23, 2023', 
+        time: '3:00 PM',
+        location: 'Villa Juliana Resort <br> <span>Tayabas, Quezon</span>',
+    }, 
+    story: {
+        title: 'Our Story', 
+        paragraphs: [
+            '<strong>The standard Lorem Ipsum passage, used since the 1500s</strong> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do e iusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea co mmodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit e sse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+            '<strong>The standard Lorem Ipsum passage, used since the 1500s</strong> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do e iusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea co mmodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit e sse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."'
+        ] 
+    }, 
+    entourage: {
+        title: 'Entourage', 
+        parents: {
+            groom: {
+                father: 'Ronnie A. Agulto',
+                mother: 'Lea S. Agulto'
+            },
+            bridge: {
+                father: 'Ricardo T. Imperial', 
+                mother: 'Ma. Juliana Imperial'
+            }
+        },
+        principals: [
+            {
+                name: 'Ericson V. Yu', 
+                gender: 'male'
+            }, 
+            {
+                name: 'Techie Manaligod',
+                gender: 'female',
+            },
+            {
+                name: 'Francis Saturnino Juan',
+                position: 'Atty.',
+                gender: 'male'
+            },
+            {
+                name: 'Minerva Hollman',
+                position: 'Dr.',
+                gender: 'female'
+            },
+            {
+                name: 'Jojo Castro', 
+                gender: 'male',
+            },
+            {
+                name: 'Mrs. Pandy', 
+                gender: 'female'
+            }, 
+            {
+                name: 'Arnold Campugan',
+                gender: 'male'
+            },
+        ],
+        secondary: [
+            {
+                name: 'Frangelico Pandy', 
+                role: 'Best Man/Scripture Reading'
+            }, 
+            {
+                name: 'Janbret Salandanan', 
+                role: 'Groomsman/Candle'
+            }, 
+            {
+                name: 'Hannah Bajao', 
+                role: 'Maid of Honor/Candle'
+            }, 
+            {
+                name: 'Juan Paolo Imperial', 
+                role: 'Groomsman/Ring Bearer'
+            }, 
+            {
+                name: 'Keith Lundgren Salandanan', 
+                role: 'Groomsman/Coin Bearer'
+            }, 
+            {
+                name: 'Jerome Atkins', 
+                role: 'Groomsman/Cord/Flower Boy'
+            },
+            {
+                name: 'Alyssa  Khyle Mendoza', 
+                role: 'Bridesmaid/Cord/Flower Girl'
+            },
+            {
+                name: 'Luigi Riego', 
+                role: 'Groomsmen/Veil'
+            },
+            {
+                name: 'Fearlyn Mae Studenski', 
+                role: 'Bridesmaid/Veil'
+            },
+            {
+                name: 'Leeyan Nicole Agulto', 
+                role: 'Bridesmaid'
+            },
+            {
+                name: 'Sydney Salvoza', 
+                role: 'Bridesmaid/Bible Bearer'
+            },
+            {
+                name: 'Regine Imperial-Del Rosario', 
+                role: 'Matron of Honor'
+            },
+        ]
+    },
+    location: {
+        title: 'Location',
+        paragraphs: [
+            '<strong>The standard Lorem Ipsum passage, used since the 1500s</strong> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do e iusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea co mmodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit e sse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        ], 
+        waze: '', 
+        maps: ''
+    }, 
+    dress: {
+        title: 'Dress Code', 
+        code: {
+            ladies: 'midi or maxi dress, jumpsuit, pantsuit Gents: dark suit, blazer, dress shirt, polo, tie',
+            mens: 'dark suit, blazer, dress shirt, polo, tie'
+        }
+    },
+    rsvp: {
+        title: 'RSVP', 
+        paragraphs: [
+            '<strong>The standard Lorem Ipsum passage, used since the 1500s</strong> <br/>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do e iusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea co mmodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit e sse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        ]
+    }
+}
