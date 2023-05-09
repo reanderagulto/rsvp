0e94a734ecc8f2bd4920960319e9bcf2645b3283
Reander Agulto <reanderagulto29@gmail.com>
2023-05-09 21:34:37 +0800

    Updated Code - Convert CSS to Tailwind CSS inside SASS File

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,45 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
+  theme: { 
     extend: {
-      color: {
+
+      dropShadow: {
+        'xl': '0.1875rem 0.3125rem 0.25rem rgba(0, 0, 0, 0.2)',
+        'xxl': '0.3125rem 0.375rem 0.25rem rgba(0, 0, 0, 0.25)'
+      },
+
+      fontFamily: {
+        primary: ['OpenSauce', 'sans-serif'],
+        secondary: ['TANAngelton', 'sans-serif'],
+        heading: ['Gistesy', 'sans-serif']
+      },
+
+      fontSize: {
+        xsm: ['1rem', '1.25rem'],
+        sm: ['1.5rem', {
+          lineHeight: '2.875rem', 
+          letterSpacing: '0.04em',
+          fontWeight: '400'
+        }],
+        md: ['1.25rem', '1.5625rem'],
+        lg: ['1.25rem', '2.5625rem'],
+        xl: ['4rem', {
+          lineHeight: '6.5625rem', 
+          letterSpacing: '0.1em', 
+          fontWeight: '400'
+        }], 
+        xxl: ['8.125rem', {
+          lineHeight:  '13.375rem', 
+          letterSpacing: '0.06em',
+          fontWeight: '400'
+        }]
+      },
+
+      color: {  
         primary: {
-          300: '#61879D',
-          500: '#446070', 
+          1000: '#61879D',
+          2000: '#446070', 
         }, 
         secondary: '#A27A3A',
         'color-swatch': {
@@ -17,27 +50,11 @@ export default {
           200: '#76B4DD',
           300: '#BEC4ED'
         },
-        'semi-white': '#F5F4F4',
-        'washed-white': '#FAF7F6',
-      }, 
-      dropShadow: {
-        'xl': '0.1875rem 0.3125rem 0.25rem rgba(0, 0, 0, 0.2);'
+        white: {
+          200: '#F5F4F4',
+          300: '#FAF7F6'
+        },
       },
-      fontSize: {
-        sm: ['16px', '20px'],
-        md: ['20px', '25px'],
-        lg: ['20px', '41px'],
-        xl: ['64px', {
-          lineHeight: '105px', 
-          letterSpacing: '0.1em', 
-          fontWeight: '400'
-        }], 
-        xxl: ['130px', {
-          lineHeight:  '214px', 
-          letterSpacing: '0.06em',
-          fontWeight: '400'
-        }]
-      }
     },
   },
   plugins: [],
