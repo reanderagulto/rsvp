466264317f0cd2536425497865d2a9c72907eadd
Reander Agulto <reanderagulto29@gmail.com>
2023-06-03 02:58:40 +0800

    Updated Code - Added Image Optimization

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 50,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 50,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 50,
+      },
+    })
+  ],
   resolve: {
     alias: {
       '@': "/src",
