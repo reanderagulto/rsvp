c28439ed8b0d290f76ef98fa0f2f91e71bdd6ac4
Reander Agulto <reanderagulto29@gmail.com>
2023-06-03 02:34:49 +0800

    Deleted old fonts and remove them in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import SplashScreen from './layout/splash'
 import './index.css'
-import '@assets/fonts/Gistesy/gistesy.ttf'
-import '@assets/fonts/TAN/tan-angelton.ttf'
-import '@assets/fonts/OpenSauce/opensauce-regular.ttf'
-import '@assets/fonts/OpenSauce/opensauce-bold.ttf'
 import Page from '@layout/page'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
