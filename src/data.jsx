ce5e90203d365015c823c49de84f8e06803264a1
Reander Agulto <reanderagulto29@gmail.com>
2023-06-09 02:10:33 +0800

    Our Story Timeline View

--- src/data.jsx
+++ src/data.jsx
@@ -18,6 +18,20 @@ import powderBlueMen from '@images/dress/powder-blue-men.jpg'
 import sageGreenLadies from '@images/dress/sage-green-ladies.jpg'
 import sageGreenMen from '@images/dress/sage-green-men.jpg'
 
+import ourStoryPandemic from '@images/our-story/our-story-pandemic.png'
+import ourStoryMoves from '@images/our-story/our-story-damoves.png'
+import ourStoryOfficial from '@images/our-story/our-story-official.png'
+import ourStoryChallenge from '@images/our-story/our-story-challenges.png'
+import ourStoryProposal from '@images/our-story/our-story-proposal.png'
+import ourStoryBeyond from '@images/our-story/our-story-beyond.png'
+
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import DirectionsRunIcon from '@mui/icons-material/DirectionsRun';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import ImportContactsIcon from '@mui/icons-material/ImportContacts';
+import DiamondIcon from '@mui/icons-material/Diamond';
+import AirplanemodeActiveIcon from '@mui/icons-material/AirplanemodeActive';
+
 export const pageImages = {
     gallery: [slide01, slide02, slide03, slide04, slide05, slide06, slide07, slide08, slide09, slide10, slide11], 
     galleryItems: [
@@ -162,16 +176,69 @@ export const pageImages = {
 }
 
 export const ourStory = {
-    story: {
-        friendship: 
+    story: [
         {
+            title: '2020 - The Pandemic Friendship',
             p: [
                 'Everything started online!!???',
                 'A very unfortunate thing happened last 2020, but somehow, it lead us to be introduced to each other on June 2020!', 
                 'Reander was serving in CCF ministry in Cavite, while Jeni was serving in CCF Main\'s B1G music ministry in Pasig. But event with that distance, people from different places of the world started serving together by collaborating online due to the pandemic.'
             ], 
-            
-        }, 
-        
-    }
+            image: ourStoryPandemic,
+            icon: <PeopleAltIcon />
+        },
+        {
+            title: '2021 - The pandemic damoves',
+            p: [
+                'If there were 2 things that we never ran out of, it would be topics to talk about and memes!!',
+                'This was the year that we got to know more about each other on a personal level. We chatted more about our lives, interests, opinions, and spent time bonding over games with our friends. We found ourselves finding interest in each other through our similarities and differences.', 
+                'Without reavealing our feelings to each other,   we found ourselves praying for each other and for our hearts to be prepared for what God has in store for us.',
+                'When things got more settled in  different aspects of our lives, Reander decided na umamin na at mag-pursue! He laid down his intentions before 2021 ended, and visited Jeni\'s parents in Quezon!'
+            ], 
+            image: ourStoryMoves,
+            icon: <DirectionsRunIcon />
+        },
+        {
+            title: 'January 23, 2022 - <br />Making it official',
+            p: [
+                'When 2023 came, naging okay yung pasok nito samin until nagkaroon ng sakit si Reander nung birthday ni Jeni na naging reason bakit di nakapunta si Reander sa quezon to celebrate Jeni\'s birthday.',
+                'Nung gumaling si Reander sa sakit niya, he went to Jeni\'s house in Quezon nung January 23, 2022. Little did Reander na yun yung time na gagawin ng official ni Jeni yung relationship nila.', 
+                'We\'re both happy that God\'s timing is perfect. He\'s never early and late, just the right time.',
+            ], 
+            image: ourStoryOfficial,
+            icon: <CheckCircleIcon />
+        },
+        {
+            title: 'The challenges of 2022',
+            p: [
+                'We all had our share of challenges in 2022. That year felt like we lost a lot-- time, health, relationships, special occasions, and a woman very special to us.',
+                'Our experiences and challenges at times, magnified our imperfections, but despite this we were committed to stay together and never give up.', 
+                'We cried together, we laughed together, we prayed together, and for each other.',
+                'The year had it\'s painful moments but there was joy because God was there with us through it all.'
+            ], 
+            image: ourStoryChallenge,
+            icon: <ImportContactsIcon />
+        },
+        {
+            title: '2023 - the proposal',
+            p: [
+                'Fast forward to 2023. Dala pa din namin yung baggage of tragedies that happened last year.',
+                'But inspite of all the tragedies/challenges di nawala yung impression ni Lord kay Reander to propose to Jeni.', 
+                'God made Reander realise that each moment that we have show God\'s love to us that\'s why even sa very low moment ni Jeni, reander propose to her to show God\'s love even in the darkest of times and even we are down.',
+            ], 
+            image: ourStoryProposal,
+            icon: <DiamondIcon />
+        },
+        {
+            title: 'september 23, 2023 <br />& the years ahead',
+            p: [
+                'We would love to spend time with you, our friends and families, as we celebrate our commitment and love for each other on Septemer 23, 2023.',
+                'Of course it never ends there! Please continue praying for our relationship, our Christ-centered marriage, our new home, and our family in the future!', 
+                'We are excited to see you!',
+            ], 
+            image: ourStoryBeyond,
+            icon: <AirplanemodeActiveIcon />
+        }
+
+    ] 
 }
--- src/sections/story/index.jsx
+++ src/sections/story/index.jsx
@@ -8,6 +8,8 @@ import './react-gallery.scss'
 import * as cx from './Story.module.scss'
 import header from '@images/header.png'
 
+import { ourStory } from '@data';
+
 // Timeline
 import Timeline from '@mui/lab/Timeline';
 import TimelineItem from '@mui/lab/TimelineItem';
@@ -17,86 +19,70 @@ import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 
-
 const Story = ({
     title, 
     paragraphs,
     gallery
 }) => {
-  return (
-    <section className={cx.wrapper} id="story">
-        <div 
-            className={cx.mask} 
-            style={{
-                backgroundImage: `url(${header})`
-            }}
-        />
-        <div className={clsx(cx.container, 'container')}>
-            <h2 className='text-center'>{title}</h2>
-            <div className={cx.content}>
-                <div className={clsx(cx.innner, cx.contentInner)}>
-                    {paragraphs.map((item, index) => {
+    return (
+        <section className={cx.wrapper} id="story">
+            <div 
+                className={cx.mask} 
+                style={{
+                    backgroundImage: `url(${header})`
+                }}
+            />
+            {/* <div className={clsx(cx.container, 'container')}>
+                <div className={cx.content}>
+                    <div className={clsx(cx.innner, cx.contentInner)}>
+                        {paragraphs.map((item, index) => {
+                            return (
+                                <p key={index}>{parse(item)}</p>
+                            )
+                        })}
+                    </div>
+                    <div className={clsx(cx.slider, cx.contentInner)}>
+                        <ImageGallery 
+                            items={gallery} 
+                            infinite={true}
+                            showNav={true}
+                            showFullscreenButton={true}
+                            showBullets={true}
+                            showPlayButton={false}
+                            autoPlay={true}
+                            slideInterval={5000}
+                        />
+                    </div>
+                </div>
+            </div> */}
+            <div className={clsx(cx.container, 'container')}>
+                <h2 className='text-center'>{title}</h2>
+                <Timeline position="alternate" className={cx.timelineContainer}>
+                    {ourStory.story.map((item, index) => {
                         return (
-                            <p key={index}>{parse(item)}</p>
+                            <TimelineItem className={cx.timelineItem} key={index}>
+                                <TimelineOppositeContent className={cx.timelineImage}>
+                                    <img src={item.image} alt={item.title} />
+                                </TimelineOppositeContent>
+                                <TimelineSeparator>
+                                    <TimelineDot>
+                                        {item.icon}
+                                    </TimelineDot>
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent className={cx.timelineContent}>
+                                    <h3>{parse(item.title)}</h3>
+                                    {item.p.map((pTags, pIndex) => {
+                                        return (<p key={pIndex}>{parse(pTags)}</p>)
+                                    })}
+                                </TimelineContent>
+                            </TimelineItem>
                         )
                     })}
-                </div>
-                <div className={clsx(cx.slider, cx.contentInner)}>
-                    <ImageGallery 
-                        items={gallery} 
-                        infinite={true}
-                        showNav={true}
-                        showFullscreenButton={true}
-                        showBullets={true}
-                        showPlayButton={false}
-                        autoPlay={true}
-                        slideInterval={5000}
-                    />
-                </div>
+                </Timeline>
             </div>
-        </div>
-        <div className={clsx(cx.container, 'container')}>
-            <Timeline position="alternate">
-                <TimelineItem>
-                    <TimelineOppositeContent color="text.secondary">
-                    09:30 am
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                        <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>Eat</TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                    <TimelineOppositeContent color="text.secondary">
-                    10:00 am
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                        <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>Code</TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                    <TimelineOppositeContent color="text.secondary">
-                    12:00 am
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                        <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>Sleep</TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                    <TimelineOppositeContent color="text.secondary">
-                    9:00 am
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                        <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>Repeat</TimelineContent>
-                </TimelineItem>
-            </Timeline>
-        </div>
-    </section>
-  )
-}
+        </section>
+    )
+    }
 
 export default Story
